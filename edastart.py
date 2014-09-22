01b115b90d518bc525fbbe4cb3d47322be85f4ba
Jari Torniainen <jtorniai@kalervo>
2014-09-22 15:24:20 +0300

    whoops had wrong filenames

--- edastart.py
+++ edastart.py
@@ -3,13 +3,13 @@
 import edanode
 from midas.dispatcher import Dispatcher
 import midas.utilities as mu
-from edastream import EDAData
+from gsrstream import GSRData
 
 
 #                       1. Start EDA LSL-Stream
 # -----------------------------------------------------------------------------
-eda_stream = EDAData(ch_count=1,srate=125,stream_name="eda_stream")
-eda_stream.start()
+gsr_stream = GSRData(ch_count=1,srate=125,stream_name="gsr_stream")
+gsr_stream.start()
 # -----------------------------------------------------------------------------
 
 
